0438be2ef407a4d6f2d7ae76d2109b7dc92bc356
Edgar Olivar <ext_emolivars@falabella.cl>
2021-11-28 16:25:00 -0300

    pre-npm ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,7 +5,6 @@ import {
   ProductTitle,
 } from '../components';
 import { products } from '../data/products';
-import '../styles/custom-style.css';
 
 const product = products[0];
 
@@ -15,27 +14,17 @@ export const ShoppingPage = () => {
       <h1>Shopping</h1>
       <ProductCard
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
           maxCount: 10,
-        }}
+        }}  
         children={({ reset, increaseBy, isMaxCountReached, count }) => (
           <>
             <ProductImage
-              className="custom-image"
               style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2' }}
             />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-
-            <button onClick={reset}>Reset!</button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(+2)}>+2</button>
-            )}
-
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            <span>{count}</span>
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       />
